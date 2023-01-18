044c86b3ff86be99210d7ffd933fd01338128213
Pippin Barr <pippin.barr@gmail.com>
2023-01-18 11:33:45 -0500

    Added s1 and s2 explainer

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ document.getElementById(`footer`)
   .addEventListener(`click`, () => {
     promo.play();
   });
+
+let lostSeasonsSuper = document.getElementById(`lost-seasons-tooltip`);
+lostSeasonsSuper.addEventListener(`click`, showTip);
+lostSeasonsSuper.addEventListener(`touchstart`, showTip);
+lostSeasonsSuper.addEventListener(`mouseover`, showTip);
+
+function showTip() {
+  document.getElementById(`lost-seasons`).style.display = `block`;
+}
